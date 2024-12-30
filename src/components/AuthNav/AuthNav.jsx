868b3c80265b27538dae2de6d053aac8e9d8ea27
Murat Çolak <mo939614@gmail.com>
2024-12-30 03:27:02 +0300

    AuthNav in

---
+++ src/components/AuthNav/AuthNav.jsx
@@ -0,0 +1,11 @@
+const AuthNav = () => {
+    return (
+      <div>
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+      </div>
+    );
+  };
+  
+  export default AuthNav;
+  
