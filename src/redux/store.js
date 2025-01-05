fac050d11c32e2b4f477982e01bbd00c47060e6f
Murat Çolak <mo939614@gmail.com>
2025-01-05 18:54:08 +0300

    store.js serialize false jsno edit.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,26 +1,31 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
 import contactsReducer from "./contacts/slice";
-import storage from "redux-persist/lib/storage"; // LocalStorage
+import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
 
+//  Redux Persist Ayarı (Token + Kullanıcı Bilgileri)
 const authPersistConfig = {
   key: "auth",
   storage,
-  whitelist: ["token", "user"],
+  whitelist: ["token", "user"], //  Kullanıcı verisini de sakla
+  serialize: false, // JSON olarak serialize etmesini engelle
 };
 
+//  Redux'un rootReducer'ı
 const rootReducer = combineReducers({
   auth: persistReducer(authPersistConfig, authReducer),
   contacts: contactsReducer,
 });
 
+//  Persist Edilmiş Reducer Kullanarak Store'u Kur
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: false, // Persist edilen veriler için kontrolü kapat
     }),
 });
 
+//  Persistor'u oluştur (Sayfa yenilendiğinde state'i korur)
 export const persistor = persistStore(store);
